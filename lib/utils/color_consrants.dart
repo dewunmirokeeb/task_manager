dfde02e5f1ca169ad05c999d215813dd4f544f9a
dewunmirokeeb <adedokunadewunmi60@gmail.com>
2022-04-03 10:31:04 +0100

    first commit

---
+++ lib/utils/color_consrants.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const maincolor = Color(0xFF202e59);
+  static const secondarycolor = Color(0xFF6c8ee3);
+  static const smalltextcolor = Color(0xFF2e3253);
+  static const textholder = Color(0xFFedf0f8);
+  static const textgrey = Color(0xFFafb1b5);
+}
